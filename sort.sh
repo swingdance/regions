2468d34d16c60368bd28928bd5547fd83d02d8b0
Kjuly <dev@kjuly.com>
2024-07-23 21:55:02 +0800

    Update sort.sh: Apply -S -I --blanks options to csvsort cmd

--- sort.sh
+++ sort.sh
@@ -21,7 +21,7 @@ if [ "$ENTITY_TYPE" = "r" ] || [ "$ENTITY_TYPE" = "region" ]; then
   echo -e "Start reordering file: $FILE ($CSV_SORT_COLUMNS)\n"
   # Error Case: RuntimeWarning: Error sniffing CSV dialect: Could not determine delimiter
   # Solution: https://csvkit.readthedocs.io/en/1.0.0/tricks.html
-  csvsort -I --snifflimit 0 -d ',' -q '"' -c "$CSV_SORT_COLUMNS" "$FILE" > "$tmp" && mv "$tmp" "$FILE"
+  csvsort -I --snifflimit 0 -d ',' -q '"' -S -I --blanks -c "$CSV_SORT_COLUMNS" "$FILE" > "$tmp" && mv "$tmp" "$FILE"
 
 # Sort /city/*.csv
 elif [ "$ENTITY_TYPE" = "c" ] || [ "$ENTITY_TYPE" = "city" ]; then
@@ -37,7 +37,7 @@ elif [ "$ENTITY_TYPE" = "c" ] || [ "$ENTITY_TYPE" = "city" ]; then
     echo -e "Start reordering file: $FILE ($CSV_SORT_COLUMNS)"
     csv_query_statement="SELECT * FROM $REGION ORDER BY pr DESC, key ASC"
     csvsql --snifflimit 0 -d ',' -q '"' -S -I --blanks --query "$csv_query_statement" "$FILE" > "$tmp" && mv "$tmp" "$FILE"
-    # csvsort -I --snifflimit 0 -d ',' -q '"' -c "$CSV_SORT_COLUMNS" "$FILE" > "$tmp" && mv "$tmp" "$FILE"
+    # csvsort -I --snifflimit 0 -d ',' -q '"' -S -I --blanks -c "$CSV_SORT_COLUMNS" "$FILE" > "$tmp" && mv "$tmp" "$FILE"
 
   else    
     echo -e "Start reordering files: /$FOLDER/*.csv ($CSV_SORT_COLUMNS)"
@@ -46,7 +46,7 @@ elif [ "$ENTITY_TYPE" = "c" ] || [ "$ENTITY_TYPE" = "city" ]; then
       table="$(basename "$csv_file" ".csv")"
       csv_query_statement="SELECT * FROM $table ORDER BY pr DESC, key ASC"
       csvsql --snifflimit 0 -d ',' -q '"' -S -I --blanks --query "$csv_query_statement" "$csv_file" > "$tmp" && mv "$tmp" "$csv_file"
-      # csvsort -I --snifflimit 0 -d ',' -q '"' -c "$CSV_SORT_COLUMNS" "$csv_file" > "$tmp" && mv "$tmp" "$csv_file"
+      # csvsort -I --snifflimit 0 -d ',' -q '"' -S -I --blanks -c "$CSV_SORT_COLUMNS" "$csv_file" > "$tmp" && mv "$tmp" "$csv_file"
     done < <(find "$FOLDER" -name "*.csv" -type f | sort)
   fi
 
