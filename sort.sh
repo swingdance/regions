7c124538ae265d2537023df1425ed6e09656d066
Kjuly <dev@kjuly.com>
2024-09-12 17:22:18 +0800

    sort.sh: Rm legacy CSV_SORT_COLUMNS; update city/us.csv

--- sort.sh
+++ sort.sh
@@ -25,28 +25,25 @@ if [ "$ENTITY_TYPE" = "r" ] || [ "$ENTITY_TYPE" = "region" ]; then
 
 # Sort /city/*.csv
 elif [ "$ENTITY_TYPE" = "c" ] || [ "$ENTITY_TYPE" = "city" ]; then
-  readonly CSV_SORT_COLUMNS="pr,key"
   readonly FOLDER="city"
-  
+
   if [ -n "$REGION" ]; then
     readonly FILE="$FOLDER/$REGION.csv"
     if [ ! -f "$FILE" ]; then
       echo -e "\n::error:: File not found: $FILE\n"
       exit 1
     fi
-    echo -e "Start reordering file: $FILE ($CSV_SORT_COLUMNS)"
+    echo -e "Start reordering file: $FILE"
     csv_query_statement="SELECT * FROM $REGION ORDER BY province ASC, pr DESC, key ASC"
     csvsql --snifflimit 0 -d ',' -q '"' -S -I --blanks --query "$csv_query_statement" "$FILE" > "$tmp" && mv "$tmp" "$FILE"
-    # csvsort -I --snifflimit 0 -d ',' -q '"' -S -I --blanks -c "$CSV_SORT_COLUMNS" "$FILE" > "$tmp" && mv "$tmp" "$FILE"
 
   else    
-    echo -e "Start reordering files: /$FOLDER/*.csv ($CSV_SORT_COLUMNS)"
+    echo -e "Start reordering files: /$FOLDER/*.csv"
     while read -r csv_file; do
       echo "- $csv_file"
       table="$(basename "$csv_file" ".csv")"
       csv_query_statement="SELECT * FROM $table ORDER BY province ASC, pr DESC, key ASC"
       csvsql --snifflimit 0 -d ',' -q '"' -S -I --blanks --query "$csv_query_statement" "$csv_file" > "$tmp" && mv "$tmp" "$csv_file"
-      # csvsort -I --snifflimit 0 -d ',' -q '"' -S -I --blanks -c "$CSV_SORT_COLUMNS" "$csv_file" > "$tmp" && mv "$tmp" "$csv_file"
     done < <(find "$FOLDER" -name "*.csv" -type f | sort)
   fi
 
